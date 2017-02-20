a059d92a1b21af0502fa2e3eb92e186717ca497e
BorisZ-git <ribos@bk.ru>
2017-02-20 18:20:18 +0300

    Continue develop form fGame\\ add btn Stop, Continue \\ add lbl Player 1,2; lbl Score 1,2

--- GuessMelody/GuessMelody/fGame.Designer.cs
+++ GuessMelody/GuessMelody/fGame.Designer.cs
@@ -31,13 +31,19 @@
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(fGame));
             this.WMP = new AxWMPLib.AxWindowsMediaPlayer();
             this.btnNext = new System.Windows.Forms.Button();
+            this.btnStop = new System.Windows.Forms.Button();
+            this.btnContinue = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.lblCounter1 = new System.Windows.Forms.Label();
+            this.lblCounter2 = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.WMP)).BeginInit();
             this.SuspendLayout();
             // 
             // WMP
             // 
             this.WMP.Enabled = true;
-            this.WMP.Location = new System.Drawing.Point(73, 268);
+            this.WMP.Location = new System.Drawing.Point(0, 351);
             this.WMP.Name = "WMP";
             this.WMP.OcxState = ((System.Windows.Forms.AxHost.State)(resources.GetObject("WMP.OcxState")));
             this.WMP.Size = new System.Drawing.Size(143, 45);
@@ -47,7 +53,7 @@
             // btnNext
             // 
             this.btnNext.Font = new System.Drawing.Font("Microsoft Sans Serif", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.btnNext.Location = new System.Drawing.Point(149, 167);
+            this.btnNext.Location = new System.Drawing.Point(12, 273);
             this.btnNext.Name = "btnNext";
             this.btnNext.Size = new System.Drawing.Size(126, 49);
             this.btnNext.TabIndex = 1;
@@ -55,12 +61,79 @@
             this.btnNext.UseVisualStyleBackColor = true;
             this.btnNext.Click += new System.EventHandler(this.btnNext_Click);
             // 
+            // btnStop
+            // 
+            this.btnStop.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.btnStop.Location = new System.Drawing.Point(165, 273);
+            this.btnStop.Name = "btnStop";
+            this.btnStop.Size = new System.Drawing.Size(112, 49);
+            this.btnStop.TabIndex = 2;
+            this.btnStop.Text = "Stop";
+            this.btnStop.UseVisualStyleBackColor = true;
+            // 
+            // btnContinue
+            // 
+            this.btnContinue.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.btnContinue.Location = new System.Drawing.Point(291, 273);
+            this.btnContinue.Name = "btnContinue";
+            this.btnContinue.Size = new System.Drawing.Size(124, 49);
+            this.btnContinue.TabIndex = 3;
+            this.btnContinue.Text = "Continue";
+            this.btnContinue.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(50, 27);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(122, 33);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Player 1";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.Location = new System.Drawing.Point(249, 27);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(122, 33);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Player 2";
+            // 
+            // lblCounter1
+            // 
+            this.lblCounter1.AutoSize = true;
+            this.lblCounter1.Font = new System.Drawing.Font("Microsoft Sans Serif", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.lblCounter1.Location = new System.Drawing.Point(89, 82);
+            this.lblCounter1.Name = "lblCounter1";
+            this.lblCounter1.Size = new System.Drawing.Size(31, 33);
+            this.lblCounter1.TabIndex = 6;
+            this.lblCounter1.Text = "0";
+            this.lblCounter1.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // lblCounter2
+            // 
+            this.lblCounter2.AutoSize = true;
+            this.lblCounter2.Font = new System.Drawing.Font("Microsoft Sans Serif", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.lblCounter2.Location = new System.Drawing.Point(285, 82);
+            this.lblCounter2.Name = "lblCounter2";
+            this.lblCounter2.Size = new System.Drawing.Size(31, 33);
+            this.lblCounter2.TabIndex = 7;
+            this.lblCounter2.Text = "0";
+            // 
             // fGame
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
             this.ClientSize = new System.Drawing.Size(422, 371);
+            this.Controls.Add(this.lblCounter2);
+            this.Controls.Add(this.lblCounter1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnContinue);
+            this.Controls.Add(this.btnStop);
             this.Controls.Add(this.btnNext);
             this.Controls.Add(this.WMP);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
@@ -68,6 +141,7 @@
             this.Text = "Game";
             ((System.ComponentModel.ISupportInitialize)(this.WMP)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -75,5 +149,11 @@
 
         private AxWMPLib.AxWindowsMediaPlayer WMP;
         private System.Windows.Forms.Button btnNext;
+        private System.Windows.Forms.Button btnStop;
+        private System.Windows.Forms.Button btnContinue;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label lblCounter1;
+        private System.Windows.Forms.Label lblCounter2;
     }
 }
--- GuessMelody/GuessMelody/fGame.cs
+++ GuessMelody/GuessMelody/fGame.cs
@@ -25,5 +25,10 @@ namespace GuessMelody
         {
             return random.Next(0, Victorina.list.Count);
         }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
